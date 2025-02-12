743b07b88f85b2fdd99bb707d5983c6cab0d82d5
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-02-12 22:46:19 +0100

    fix bug

--- app/components/header/HeaderV2.vue
+++ app/components/header/HeaderV2.vue
@@ -25,7 +25,7 @@
             Premium Manicure & Nail Art in
           </h2>
 
-          <ButtonV2 to="/open">Book Appointment</ButtonV2>
+          <ButtonV2>Book Appointment</ButtonV2>
         </div>
       </div>
     </div>
