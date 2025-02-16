334659d61c9fac756da1a39962eb53521e10620a
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-02-16 18:49:28 +0100

    config schema2

---
+++ cms/schemaTypes/config.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'config',
+  title: 'Site Configuration',
+  type: 'document',
+  fields: [
+    {
+      name: 'favicon',
+      title: 'Favicon',
+      type: 'image',
+    },
+  ],
+}
