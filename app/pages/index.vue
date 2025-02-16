36a79c47dcd0d45aed7d84b8f837d68f9a0ca9ad
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-02-16 12:21:39 +0100

    seo from sanity to page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,29 +10,35 @@
 </template>
 
 <script setup>
+const { data } = await useSanityQuery(`*[_type == "page"][0] {
+  title,
+  seo,
+  services
+}`);
+
 useSeoMeta({
-  title: 'Sissi Nails',
-  description: 'Luxury nail care with a personalized touch.',
-  ogTitle: 'Sissi Nails',
-  ogDescription: 'Luxury nail care with a personalized touch.',
-  ogImage: '/images/nails.png',
-  ogUrl: `http:localhost:3000/`,
-  twitterTitle: 'Sissi Nails',
-  twitterDescription: 'Luxury nail care with a personalized touch.',
-  twitterImage: '/images/nails.png',
-  twitterCard: 'Summary',
+  title: data.value.seo.metaTitle,
+  description: data.value.seo.metaDescription,
+  ogTitle: data.value.seo.metaTitle,
+  ogDescription: data.value.seo.metaDescription,
+  ogImage: data.value.seo.ogImage,
+  ogUrl: data.value.seo.ogUrl,
+  twitterTitle: data.value.seo.metaTitle,
+  twitterDescription: data.value.seo.metaDescription,
+  twitterImage: data.value.seo.ogImage,
+  twitterCard: data.value.seo.card,
 });
 
 useHead({
   htmlAttrs: {
     lang: 'en',
   },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png',
-    },
-  ],
+  // link: [
+  //   {
+  //     rel: 'icon',
+  //     type: 'image/png',
+  //     href: '/favicon.png',
+  //   },
+  // ],
 });
 </script>
